1e0495402491f948f14043534dfd5acd0649fd07
michael tuohy <michael.tuohy@webworx.ie>
2025-09-07 22:07:51 +0100

    remove-comments

--- server/server.go
+++ server/server.go
@@ -9,17 +9,14 @@ import (
 	"github.com/webworx-mt/oauth/router"
 )
 
-// StartServer starts the HTTP server
 func StartServer(port string) {
 	// Create router
 	r := router.New()
 
-	// Define routes with REST conventions
 	r.GET("/health", handlers.HealthHandler)
 	r.GET("/users", handlers.GetUsersHandler)
 	r.GET("/user/:id", handlers.GetUserHandler)
 
-	// Start server
 	fmt.Printf("Server starting on port %s\n", port)
 
 	log.Fatal(http.ListenAndServe(port, r))
